56b73623c09e6664c3c6abcd43cd01e9776b719d
maximp <maximp@localhost>
2016-09-01 21:00:49 +0200

    assignment 1 - svm

--- gist/c231n-convnets-2016/assignment-1/assignment1/cs231n/classifiers/linear_classifier.py
+++ gist/c231n-convnets-2016/assignment-1/assignment1/cs231n/classifiers/linear_classifier.py
@@ -3,7 +3,6 @@ from cs231n.classifiers.linear_svm import *
 from cs231n.classifiers.softmax import *
 
 class LinearClassifier(object):
-
   def __init__(self):
     self.W = None
 
@@ -35,38 +34,15 @@ class LinearClassifier(object):
     # Run stochastic gradient descent to optimize W
     loss_history = []
     for it in xrange(num_iters):
-      X_batch = None
-      y_batch = None
-
-      #########################################################################
-      # TODO:                                                                 #
-      # Sample batch_size elements from the training data and their           #
-      # corresponding labels to use in this round of gradient descent.        #
-      # Store the data in X_batch and their corresponding labels in           #
-      # y_batch; after sampling X_batch should have shape (dim, batch_size)   #
-      # and y_batch should have shape (batch_size,)                           #
-      #                                                                       #
-      # Hint: Use np.random.choice to generate indices. Sampling with         #
-      # replacement is faster than sampling without replacement.              #
-      #########################################################################
-      pass
-      #########################################################################
-      #                       END OF YOUR CODE                                #
-      #########################################################################
+      idx = np.random.choice(num_train, batch_size, replace=True)
+      X_batch = X[idx, :]
+      y_batch = y[idx]
 
       # evaluate loss and gradient
       loss, grad = self.loss(X_batch, y_batch, reg)
       loss_history.append(loss)
-
       # perform parameter update
-      #########################################################################
-      # TODO:                                                                 #
-      # Update the weights using the gradient and the learning rate.          #
-      #########################################################################
-      pass
-      #########################################################################
-      #                       END OF YOUR CODE                                #
-      #########################################################################
+      self.W -= learning_rate * grad
 
       if verbose and it % 100 == 0:
         print 'iteration %d / %d: loss %f' % (it, num_iters, loss)
@@ -86,15 +62,7 @@ class LinearClassifier(object):
       array of length N, and each element is an integer giving the predicted
       class.
     """
-    y_pred = np.zeros(X.shape[1])
-    ###########################################################################
-    # TODO:                                                                   #
-    # Implement this method. Store the predicted labels in y_pred.            #
-    ###########################################################################
-    pass
-    ###########################################################################
-    #                           END OF YOUR CODE                              #
-    ###########################################################################
+    y_pred = np.argmax(X.dot(self.W), axis=1)
     return y_pred
   
   def loss(self, X_batch, y_batch, reg):
@@ -127,4 +95,3 @@ class Softmax(LinearClassifier):
 
   def loss(self, X_batch, y_batch, reg):
     return softmax_loss_vectorized(self.W, X_batch, y_batch, reg)
-
