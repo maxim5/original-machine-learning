5890cdc6b3d94d44a25f589587928cd8b87d42c0
Maxim Podkolzine <smartmaxim@gmail.com>
2016-10-13 20:48:39 +0200

    more flexible hyper tune

--- research/image_classification/tf/experiment_2.py
+++ research/image_classification/tf/experiment_2.py
@@ -14,17 +14,13 @@ from conv_model import ConvModel
 default_hyper_params = {
   'batch_size': 128,
   'epochs': 10,
-  'init_stdev': 0.01,
+  'init_stdev': 0.05,
   'learning_rate': 0.001,
 
   'conv_filters': [
-    # [[7, 1,  32], [1, 7,  32]],
-    # [[7, 1,  64], [1, 7,  64]],
-    # [[3, 1, 96], [1, 3, 96]],
-
-    [[3, 3, 32]],
-    [[3, 3, 64]],
-    [[3, 3, 256]],
+    [[1, 5,  36], [5, 1,  36]],
+    [[1, 5,  64], [5, 1,  64]],
+    [[1, 5, 256], [5, 1, 256]],
   ],
   'conv_pools': [
     [2, 2]
@@ -44,8 +40,8 @@ def random_conv_spec():
   size3 = np.random.choice([3, 5])
 
   num1 = np.random.choice([24, 32, 36])
-  num2 = np.random.choice([32, 48, 64, 72])
-  num3 = np.random.choice([0, 64, 72, 96, 128, 256])
+  num2 = np.random.choice([64, 96, 128])
+  num3 = np.random.choice([128, 256, 512])
 
   spec = [
     [[size1, size1, num1]],
@@ -61,21 +57,25 @@ def save_hyper(accuracy, hyper_params, path='best-hyper-%.4f.txt', limit=0.992):
 
   filename = path % limit
   with open(filename, 'a') as file_:
-    file_.write('accuracy=%.4f -> %s' % (accuracy, str(hyper_params)))
+    file_.write('accuracy=%.4f -> %s\n' % (accuracy, dict_to_str(hyper_params)))
 
 
 def hyper_tune(data_sets, model):
   best_accuracy = 0
   while True:
     hyper_params = default_hyper_params.copy()
-    hyper_params['epochs'] = 12
-    # accuracy=0.9618, tuned_params:  {'init_stdev': 0.05529920193609984}
+    hyper_params['epochs'] = 10
     tuned_params = {
       'init_stdev': np.random.uniform(0.04, 0.06),
       'learning_rate': 10**np.random.uniform(-2, -4),
-      'dropout_conv': np.random.uniform(0.5, 1.0),
-      'dropout_fc': np.random.uniform(0.2, 1.0),
+
       'conv_filters': random_conv_spec(),
+      'conv_activation': np.random.choice(['relu', 'elu']),
+      'conv_dropout': np.random.uniform(0.7, 1.0),
+
+      'fc_size': np.random.choice([512, 768, 1024, 2048]),
+      'fc_activation': np.random.choice(['relu', 'elu']),
+      'fc_dropout': np.random.uniform(0.5, 1.0),
     }
     hyper_params.update(tuned_params)
 
@@ -86,14 +86,14 @@ def hyper_tune(data_sets, model):
     if accuracy > best_accuracy:
       best_accuracy = accuracy
       marker = '!!!'
-    log("%s accuracy=%.4f, tuned_params: " %  (marker, accuracy), tuned_params)
+    log('%s accuracy=%.4f, tuned_params: %s' % (marker, accuracy, dict_to_str(tuned_params)))
 
     save_hyper(accuracy, hyper_params)
 
 
 def train_best_candidate(data_sets, model):
   hyper_params = default_hyper_params.copy()
-  hyper_params.update({'learning_rate': 0.0006568582429913265, 'dropout_fc': 0.8558553724757245, 'dropout_conv': 0.9400811432728358, 'init_stdev': 0.043707865797119355})
+  hyper_params.update({'batch_size': 128, 'conv_activation': 'elu', 'conv_dropout': 0.79266, 'conv_filters': [[[5, 5, 24]], [[3, 3, 64]], [[5, 5, 256]]], 'conv_pools': [[2, 2]], 'epochs': 10, 'fc_activation': 'relu', 'fc_dropout': 0.85501, 'fc_size': 1024, 'init_stdev': 0.05557, 'learning_rate': 0.00108})
   hyper_params['epochs'] = 30
   train(data_sets=data_sets, model=model, **hyper_params)
 
@@ -105,4 +105,4 @@ def train_default(data_sets, model):
 if __name__ == "__main__":
   mnist = input_data.read_data_sets("../../../dat/mnist-tf", one_hot=True)
   conv_model = ConvModel(input_shape=(28, 28, 1), num_classes=10)
-  train_default(mnist, conv_model)
+  hyper_tune(mnist, conv_model)
