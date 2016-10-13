819e419e18460867b5dffa879d8724be24019073
maximp <maximp@localhost>
2016-10-13 13:21:57 +0200

    simplify code

--- research/image_classification/tf/common.py
+++ research/image_classification/tf/common.py
@@ -27,12 +27,11 @@ def is_gpu():
 def total_params():
   total_parameters = 0
   for variable in tf.trainable_variables():
-      # shape is an array of tf.Dimension
       shape = variable.get_shape()
-      variable_parametes = 1
+      variable_parameters = 1
       for dim in shape:
-          variable_parametes *= dim.value
-      total_parameters += variable_parametes
+          variable_parameters *= dim.value
+      total_parameters += variable_parameters
   return total_parameters
 
 
