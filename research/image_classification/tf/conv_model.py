4adbfce96860955732c49dca64ea248b62763727
Maxim Podkolzine <2580604+maxim5@users.noreply.github.com>
2016-10-14 21:25:46 +0200

    make hyper params hierarchical

--- research/image_classification/tf/conv_model.py
+++ research/image_classification/tf/conv_model.py
@@ -6,8 +6,6 @@ __author__ = "maxim"
 import tensorflow as tf
 import tflearn
 
-from util import zip_longest
-
 
 class ConvModel:
   def __init__(self, input_shape, num_classes):
@@ -19,27 +17,28 @@ class ConvModel:
     return tf.random_normal(shape) * self.hyper_params['init_stdev']
 
 
-  def get_activation_function(self, name):
+  def _get_activation_function(self, name):
     return getattr(tflearn.activations, name, None)
 
 
-  def conv2d_relu(self, image, W, b, strides):
-    activation = self.get_activation_function(self.hyper_params.get('conv_activation', 'relu'))
+  def conv2d_activation(self, image, W, b, strides, params):
+    activation_func = self._get_activation_function(params['activation'] or 'relu')
     layer = tf.nn.conv2d(image, W, strides=[1, strides, strides, 1], padding='SAME')
     layer = tf.nn.bias_add(layer, b)
-    layer = activation(layer)
+    layer = activation_func(layer)
     return layer
 
 
-  def conv_layer(self, image, filter_size, pool_size, dropout):
+  def conv_layer(self, image, params):
     conv = image
-    for filter in filter_size:
+    for filter in params['filters_adapted']:
       W = tf.Variable(self.init(filter))
       b = tf.Variable(self.init(filter[-1:]))
-      conv = self.conv2d_relu(conv, W, b, strides=1)
+      conv = self.conv2d_activation(conv, W, b, strides=1, params=params)
 
-    layer = tf.nn.max_pool(conv, ksize=pool_size, strides=pool_size, padding='SAME')
-    layer = tf.nn.dropout(layer, keep_prob=dropout)
+    layer = tf.nn.max_pool(conv, ksize=params['pools_adapted'], strides=params['pools_adapted'], padding='SAME')
+    if self.mode == 'train':
+      layer = tf.nn.dropout(layer, keep_prob=params['dropout'])
     return layer
 
 
@@ -49,18 +48,18 @@ class ConvModel:
     return layer
 
 
-  def fully_connected_layer(self, input, size, dropout):
+  def fully_connected_layer(self, input, size, params):
     input_shape = input.get_shape()
     fc_shape = [input_shape[1].value * input_shape[2].value * input_shape[3].value, size]
     W = tf.Variable(self.init(fc_shape))
     b = tf.Variable(self.init(fc_shape[-1:]))
 
-    activation = self.get_activation_function(self.hyper_params.get('fc_activation', 'relu'))
-
+    activation_func = self._get_activation_function(params['activation'] or 'relu')
     layer = tf.reshape(input, [-1, W.get_shape().as_list()[0]])
     layer = tf.add(tf.matmul(layer, W), b)
-    layer = activation(layer)
-    layer = tf.nn.dropout(layer, dropout)
+    layer = activation_func(layer)
+    if self.mode == 'train':
+      layer = tf.nn.dropout(layer, keep_prob=params['dropout'])
     return layer
 
 
@@ -71,33 +70,39 @@ class ConvModel:
     return layer
 
 
-  def adapt_shapes(self, filters, pools):
+  def _adapt_conv_shapes(self, conv_params, conv_layers_num):
     channels = self.input_shape[-1]
-    result_filters, result_pools = [], []
-    for filter_layer, pool in zip_longest(filters, pools):
-      adapted_filters = []
-      for filter in filter_layer:
-        adapted_filters.append([filter[0], filter[1], channels, filter[2]])
+    for i in xrange(1, conv_layers_num + 1):
+      layer_params = conv_params[i]
+
+      adapted = []
+      for filter in layer_params['filters']:
+        adapted.append([filter[0], filter[1], channels, filter[2]])
         channels = filter[2]
-      result_filters.append(adapted_filters)
-      result_pools.append([1, pool[0], pool[1], 1])
-    return result_filters, result_pools
+      layer_params['filters_adapted'] = adapted
+
+      pools = layer_params['pools']
+      layer_params['pools_adapted'] = [1, pools[0], pools[1], 1]
 
 
   def conv_net(self):
-    self.dropout_conv = tf.placeholder(tf.float32)
-    self.dropout_fc = tf.placeholder(tf.float32)
+    # Input
+    image_shape = (-1,) + tuple(self.input_shape)
+    layer_input = tf.reshape(self.x, shape=image_shape)
 
-    image_shape = (-1,) + self.input_shape
-    layer_conv = tf.reshape(self.x, shape=image_shape)
+    # Conv layers
+    conv_params = self.hyper_params['conv']
+    conv_layers_num = conv_params['layers_num']
+    self._adapt_conv_shapes(conv_params, conv_layers_num)
+    layer_conv = layer_input
+    for i in xrange(1, conv_layers_num + 1):
+      layer_conv = self.conv_layer(layer_conv, params=conv_params[i])
 
-    filters, pools = self.adapt_shapes(self.hyper_params['conv_filters'], self.hyper_params['conv_pools'])
-    for filter, pool in zip(filters, pools):
-      layer_conv = self.conv_layer(layer_conv, filter_size=filter, pool_size=pool, dropout=self.dropout_conv)
-
+    # Reduced + fully-connected + output layers
+    fc_params = self.hyper_params['fc']
     layer_pool = self.reduce_layer(layer_conv)
-    layer_fc = self.fully_connected_layer(layer_pool, size=self.hyper_params['fc_size'], dropout=self.dropout_fc)
-    layer_out = self.output_layer(layer_fc, shape=[self.hyper_params['fc_size'], self.num_classes])
+    layer_fc = self.fully_connected_layer(layer_pool, size=fc_params['size'], params=fc_params)
+    layer_out = self.output_layer(layer_fc, shape=[fc_params['size'], self.num_classes])
 
     return layer_out
 
@@ -106,6 +111,7 @@ class ConvModel:
     self.hyper_params = hyper_params
     self.x = tf.placeholder(tf.float32, [None, self.input_shape[0] * self.input_shape[1] * self.input_shape[2]])
     self.y = tf.placeholder(tf.float32, [None, self.num_classes])
+    self.mode = tf.placeholder(tf.string)
 
     prediction = self.conv_net()
     cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(prediction, self.y))
@@ -129,7 +135,7 @@ class ConvModel:
     return total_parameters
 
 
-  def feed_dict(self, data_set=None, images=None, labels=None, **hyper_params):
+  def feed_dict(self, data_set=None, images=None, labels=None, mode='test'):
     if images is None and data_set is not None:
       images = data_set.images
     if labels is None and data_set is not None:
@@ -137,6 +143,5 @@ class ConvModel:
     return {
       self.x: images,
       self.y: labels,
-      self.dropout_conv: hyper_params.get('conv_dropout', 1.0),
-      self.dropout_fc: hyper_params.get('fc_dropout', 1.0),
+      self.mode: mode,
     }
