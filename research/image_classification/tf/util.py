0d1a3b650f313f0e946f0b25d50bfd7193b8dd64
maximp <maximp@localhost>
2016-10-14 16:18:39 +0200

    extract Solver and HyperTuner

---
+++ research/image_classification/tf/util.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+__author__ = "maxim"
+
+
+def dict_to_str(d):
+  def smart_str(val):
+    if type(val) == float:
+      return "%.5f" % val
+    return repr(val)
+
+  return '{%s}' % ', '.join(['%s: %s' % (repr(k), smart_str(d[k])) for k in sorted(d.keys())])
+
+
+def zip_longest(list1, list2):
+  len1 = len(list1)
+  len2 = len(list2)
+  for i in xrange(max(len1, len2)):
+    yield (list1[i % len1], list2[i % len2])
