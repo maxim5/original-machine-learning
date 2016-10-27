95255bd32d70937b0e16f2d75b565f5c685085c3
Maxim Podkolzine <2580604+maxim5@users.noreply.github.com>
2016-10-27 18:50:59 +0200

    save misclassified data along with session and hyper params

--- research/image_classification/tf/model_io.py
+++ research/image_classification/tf/model_io.py
@@ -13,6 +13,7 @@ class ModelIO(Logger):
     super(ModelIO, self).__init__(log_level)
     self.load_dir = params.get('load_dir')
     self.save_dir = params.get('save_dir')
+    self.data_saver = params.get('data_saver')
 
 
   def save_results(self, results, directory=None):
@@ -27,13 +28,13 @@ class ModelIO(Logger):
 
 
   def load_results(self, directory, log_level):
-    if not directory:
+    if directory is None:
       return
 
-    results_file = os.path.join(directory, 'results.xjson')
-    if os.path.exists(results_file):
-      results = ModelIO._load_dict(results_file)
-      self._log(log_level, 'Loaded results: %s from %s' % (dict_to_str(results), results_file))
+    destination = os.path.join(directory, 'results.xjson')
+    if os.path.exists(destination):
+      results = ModelIO._load_dict(destination)
+      self._log(log_level, 'Loaded results: %s from %s' % (dict_to_str(results), destination))
       return results
 
 
@@ -50,7 +51,7 @@ class ModelIO(Logger):
 
   def load_hyper_params(self, directory=None):
     directory = directory or self.load_dir
-    if not directory:
+    if directory is None:
       return
 
     hyper_params = ModelIO._load_dict(os.path.join(directory, 'hyper_params.xjson'))
@@ -59,9 +60,22 @@ class ModelIO(Logger):
       return hyper_params
 
 
+  def save_data(self, x, y, directory=None):
+    directory = directory or self.save_dir
+    if self.data_saver is None or x is None or y is None or not ModelIO._prepare(directory):
+      return
+
+    destination = os.path.join(directory, 'misclassified')
+    actual_destination = call(self.data_saver, x, y, destination)
+    if actual_destination:
+      self.debug('Misclassified data saved to %s' % actual_destination)
+    else:
+      self.warn('Data saver can not be not called or returns None')
+
+
   @staticmethod
   def _prepare(directory):
-    if not directory:
+    if directory is None:
       return False
     if not os.path.exists(directory):
       os.makedirs(directory)
