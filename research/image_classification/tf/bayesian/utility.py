b476f19853873263ac6a2de47f6a2f16652ed582
Maxim Podkolzine <2580604+maxim5@users.noreply.github.com>
2016-11-05 12:45:25 +0100

    add a prior mu

--- research/image_classification/tf/bayesian/utility.py
+++ research/image_classification/tf/bayesian/utility.py
@@ -24,22 +24,23 @@ class BaseUtility(object):
 
 
 class BaseGaussianUtility(BaseUtility):
-  def __init__(self, points, values, kernel, **params):
+  def __init__(self, points, values, kernel, mu_prior=0, **params):
     super(BaseGaussianUtility, self).__init__(points, values)
     self.kernel = kernel
+    self.mu_prior = mu_prior
 
     kernel_matrix = self.kernel.compute(self.points)
     self.k_inv = np.linalg.pinv(kernel_matrix)
-    self.k_inv_f = np.dot(self.k_inv, self.values)
+    self.k_inv_f = np.dot(self.k_inv, (self.values-self.mu_prior))
 
-  def _mean_and_std(self, batch):
+  def mean_and_std(self, batch):
     assert len(batch.shape) == 2
 
     batch = np.array(batch)
     k_star = np.swapaxes(self.kernel.compute(self.points, batch), 0, 1)
     k_star_star = self.kernel.id(batch)
 
-    mu_star = np.dot(k_star, self.k_inv_f)
+    mu_star = self.mu_prior + np.dot(k_star, self.k_inv_f)
 
     t_star = np.dot(self.k_inv, k_star.T)
     t_star = np.einsum('ij,ji->i', k_star, t_star)
@@ -49,13 +50,13 @@ class BaseGaussianUtility(BaseUtility):
 
 
 class ProbabilityOfImprovement(BaseGaussianUtility):
-  def __init__(self, points, values, kernel, **params):
-    super(ProbabilityOfImprovement, self).__init__(points, values, kernel, **params)
+  def __init__(self, points, values, kernel, mu_prior=0, **params):
+    super(ProbabilityOfImprovement, self).__init__(points, values, kernel, mu_prior, **params)
     self.epsilon = params.get('epsilon', 1e-8)
     self.max_value = np.max(self.values)
 
   def compute_values(self, batch):
-    mu, sigma = self._mean_and_std(batch)
+    mu, sigma = self.mean_and_std(batch)
     z = (mu - self.max_value - self.epsilon) / sigma
     cdf = stats.norm.cdf(z)
     cdf[np.abs(mu - self.max_value) < self.epsilon] = 0.0
