02618f07639915b1f255fe2d0e8fef3019da1063
Maxim Podkolzine <smartmaxim@gmail.com>
2016-11-08 15:25:47 +0100

    make a params for tweak probabilities

--- research/image_classification/tf/bayesian/maximizer.py
+++ research/image_classification/tf/bayesian/maximizer.py
@@ -21,6 +21,8 @@ class MonteCarloUtilityMaximizer(BaseUtilityMaximizer):
     super(MonteCarloUtilityMaximizer, self).__init__(utility)
     self.sampler = sampler
     self.batch_size = params.get('batch_size', 100000)
+    self.tweak_probability = params.get('tweak_probability', 0)
+    self.flip_probability = params.get('flip_probability', 0)
 
   def compute_max_point(self):
     batch = self.sampler.sample(size=self.batch_size)
@@ -29,9 +31,9 @@ class MonteCarloUtilityMaximizer(BaseUtilityMaximizer):
     log('Max prediction_value: %.6f' % values[i])
     return self._tweak_randomly(batch[i], batch[0])
 
-  def _tweak_randomly(self, optimal, point, tweak_probability=0.0):
-    if np.random.uniform() < tweak_probability:
-      p = min(2.0 / optimal.shape[0], 0.5)
+  def _tweak_randomly(self, optimal, point):
+    if np.random.uniform() < self.tweak_probability:
+      p = self.flip_probability or min(2.0 / optimal.shape[0], 0.5)
       mask = np.random.choice([False, True], size=optimal.shape, p=[1-p, p])
       optimal[mask] = point[mask]
     return optimal
