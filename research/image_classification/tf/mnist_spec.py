ce70ec33786187c712240a88ca77d6378a034770
Maxim Podkolzine <2580604+maxim5@users.noreply.github.com>
2016-11-13 14:24:06 +0100

    update spec, save first results

--- research/image_classification/tf/mnist_spec.py
+++ research/image_classification/tf/mnist_spec.py
@@ -7,13 +7,13 @@ from image_classification.tf import spec
 
 
 def random_conv_layer(size, num, prob=0.8):
-  def if_cond(switch, size, num):
-    if switch > prob:
-      return [[size, 1, num], [1, size, num]]
-    return [[size, size, num]]
-
-  return spec.merge(if_cond, spec.uniform(), size, num)
-
+  # def if_cond(switch, size, num):
+  #   if switch > prob:
+  #     return [[size, 1, num], [1, size, num]]
+  #   return [[size, size, num]]
+  #
+  # return spec.merge(if_cond, spec.uniform(), size, num)
+  return [[size, size, num]]
 
 activations = ['relu', 'relu6', 'elu', 'prelu', 'leaky_relu']
 
@@ -21,11 +21,11 @@ hyper_params = {
   'init_stdev': spec.uniform(0.04, 0.06),
 
   'augment': {
-    'scale': [spec.min_(spec.uniform(0.9, 1.1), 1.0),
-              spec.max_(spec.uniform(0.9, 1.1), 1.0)],
-    'rotation_angle': spec.choice([0, spec.uniform(0, 20)]),
-    'blur_sigma': spec.choice([0, 10**spec.uniform(-2, 0.2)]),
-    'crop_size': spec.choice(range(3)),
+  #   'scale': [spec.min_(spec.uniform(0.9, 1.1), 1.0),
+  #             spec.max_(spec.uniform(0.9, 1.1), 1.0)],
+  #   'rotation_angle': spec.choice([0, spec.uniform(0, 20)]),
+  #   'blur_sigma': spec.choice([0, 10**spec.uniform(-2, 0.2)]),
+  #   'crop_size': spec.choice(range(3)),
   },
 
   'optimizer': {
@@ -38,19 +38,19 @@ hyper_params = {
   'conv': {
     'layers_num': 3,
     1: {
-      'filters': random_conv_layer(size=spec.choice([3, 5, 7]), num=spec.choice([ 24,  32,  36])),
+      'filters': random_conv_layer(size=spec.choice(range(3, 8)), num=spec.choice(range(24, 37))),
       'pools': [2, 2],
       'activation': spec.choice(activations),
       'dropout': spec.uniform(0.85, 1.0),
     },
     2: {
-      'filters': random_conv_layer(size=spec.choice([3, 5,  ]), num=spec.choice([ 64,  96, 128])),
+      'filters': random_conv_layer(size=spec.choice(range(3, 6)), num=spec.choice(range(64, 129))),
       'pools': [2, 2],
       'activation': spec.choice(activations),
       'dropout': spec.uniform(0.8, 1.0),
     },
     3: {
-      'filters': random_conv_layer(size=spec.choice([3, 5,  ]), num=spec.choice([128, 256, 512])),
+      'filters': random_conv_layer(size=spec.choice(range(3, 6)), num=spec.choice(range(128, 513))),
       'pools': [2, 2],
       'activation': spec.choice(activations),
       'dropout': spec.uniform(0.6, 1.0),
@@ -58,7 +58,7 @@ hyper_params = {
   },
 
   'fc': {
-    'size': spec.choice([512, 768, 1024]),
+    'size': spec.choice(range(512, 1025)),
     'activation': spec.choice(activations),
     'dropout': spec.uniform(0.5, 1.0),
   },
