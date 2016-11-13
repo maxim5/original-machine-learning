053c87202295db8710173342fb547d3a46a7b3fa
Maxim Podkolzine <2580604+maxim5@users.noreply.github.com>
2016-11-13 16:04:30 +0100

    improve the hyper-tuner

--- research/image_classification/tf/hyper_tuner.py
+++ research/image_classification/tf/hyper_tuner.py
@@ -19,7 +19,6 @@ def tf_reset_all():
 class HyperTuner(Logger):
   def __init__(self, hyper_params_spec, solver_generator, **strategy_params):
     super(HyperTuner, self).__init__(1)
-    self.best_accuracy = 0
     self.solver_generator = solver_generator
 
     self.parsed = ParsedSpec(hyper_params_spec)
@@ -29,25 +28,23 @@ class HyperTuner(Logger):
     sampler.add_uniform(self.parsed.size())
     self.strategy = BayesianStrategy(sampler, **strategy_params)
 
-
-  def tune(self, ):
+  def tune(self):
     self.info('Start hyper-tuner')
 
-    trial = 0
     while True:
       point = self.strategy.next_proposal()
       hyper_params = self.parsed.instantiate(point)
       solver = self.solver_generator(hyper_params)
-      trial += 1
+
       tf_reset_all()
       accuracy = solver.train()
-      self._update_accuracy(trial, accuracy, hyper_params)
+
+      previous_max = max(self.strategy.values)
       self.strategy.add_point(point, accuracy)
+      index = len(self.strategy.values)
 
-
-  def _update_accuracy(self, trial, accuracy, tuned_params):
-    marker = ' '
-    if accuracy > self.best_accuracy:
-      self.best_accuracy = accuracy
-      marker = '!'
-    self.info('%s [%d] accuracy=%.4f, params: %s' % (marker, trial, accuracy, dict_to_str(tuned_params)))
+      marker = '!' if accuracy > previous_max else ' '
+      self.info('%s [%d] accuracy=%.4f, params: %s' % (marker, index, accuracy, dict_to_str(hyper_params)))
+      self.info('Current top-5:')
+      for value in sorted(self.strategy.values, reverse=True)[:5]:
+        self.info('  accuracy=%.4f' % value)
