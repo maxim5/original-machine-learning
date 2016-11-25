4bd30208564b8f7e6888c8663b817df5946bbdcb
Maxim Podkolzine <smartmaxim@gmail.com>
2016-11-25 21:43:45 +0100

    fix curve predictor

--- research/image_classification/tf/curve_predictor.py
+++ research/image_classification/tf/curve_predictor.py
@@ -6,7 +6,7 @@ import os
 import numpy as np
 from image_classification.tf.base_io import BaseIO
 from image_classification.tf.log import log
-from image_classification.tf.util import dict_to_str
+from image_classification.tf.util import dict_to_str, slice_dict
 
 __author__ = "maxim"
 
@@ -46,7 +46,7 @@ class BaseCurvePredictor(object):
     self._burn_in = params.get('burn_in', 10)
     self._min_input_size = params.get('min_input_size', 3)
 
-    self._curve_io = CurvePredictorIO(**params)
+    self._curve_io = CurvePredictorIO(self, **slice_dict(params, 'io_'))
     self._curve_io.load()
 
   @property
@@ -72,7 +72,7 @@ class BaseCurvePredictor(object):
       self._x = np.concatenate([self._x, curve], axis=0)
       self._y = np.concatenate([self._y, value], axis=0)
 
-    log('Adding curve. Current data shape: ', self._x.shape)
+    log('Adding curve (value=%.4f). Current data shape: ' % value[0], self._x.shape)
     self._curve_io.save()
 
   def predict(self, curve):
@@ -96,9 +96,9 @@ class BaseCurvePredictor(object):
 
 
 class LinearCurvePredictor(BaseCurvePredictor):
-  def __init__(self, value_limit, **params):
+  def __init__(self, **params):
     super(LinearCurvePredictor, self).__init__(**params)
-    self._value_limit = value_limit
+    self._value_limit = params.get('value_limit')
     self._model = {}
 
   def add_curve(self, curve, value):
@@ -106,32 +106,39 @@ class LinearCurvePredictor(BaseCurvePredictor):
     self._model = {}
 
   def predict(self, curve):
-    curve = np.array(curve)
     size = curve.shape[0]
-    if self.curves_number < self._burn_in or size < self._min_input_size or size >= self.curve_length:
+    if self.curves_number < self._burn_in or size < self._min_input_size or size >= self.curves_number:
       return None
 
     w, error = self._build_model(size)
     value_prediction = curve[:size].dot(w)
-    log('Prediction for curve %s: %.4f (error=%.4f)' % (str(curve), value_prediction, error))
-    return value_prediction-error, value_prediction, value_prediction+error
+    log('Prediction for the curve: %.4f (error=%.4f)' % (value_prediction, error))
+    return value_prediction - 2*error, value_prediction, value_prediction + 2*error
 
   def stop_condition(self):
     def condition(curve):
+      curve = np.array(curve)
       interval = self.predict(curve)
       if interval:
         _, _, right = interval
-        if right < self._value_limit:
+        limit = self._value_limit or np.max(self._y)
+        if right < limit:
+          log('Max expected value for the curve is %.4f. Stop now (curve size = %d / %d)' %
+              (right, curve.shape[0], self.curve_length))
           return True
       return False
     return condition
 
   def result_metric(self):
     def metric(curve):
-      if len(curve) < self.curve_length:
+      curve = np.array(curve)
+      if curve.shape[0] < self.curve_length:
         _, expected, _ = self.predict(curve)
+        log('Expected value for the curve is %.4f' % expected)
         return expected
-      return max(curve)
+      value = max(curve)
+      self.add_curve(curve, value)
+      return value
     return metric
 
   def _build_model(self, size):
@@ -153,4 +160,4 @@ class LinearCurvePredictor(BaseCurvePredictor):
     x = self._x[:,:size]
     y = self._y
     predictions = x.dot(w)
-    return np.sqrt(np.mean((predictions - y)**2))
+    return np.max(np.abs(predictions - y))
