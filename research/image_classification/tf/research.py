4fe8690c6dc8df3b57e361bacd19c24bc91f5ac7
Maxim Podkolzine <smartmaxim@gmail.com>
2016-11-25 14:53:18 +0100

    explore cifar-10 hyper-space (v1.1)

--- research/image_classification/tf/research.py
+++ research/image_classification/tf/research.py
@@ -3,8 +3,8 @@
 from image_classification.tf.bayesian.artist import Artist
 from image_classification.tf.bayesian.sampler import DefaultSampler
 from image_classification.tf.bayesian.strategy import BayesianStrategy
+from image_classification.tf.cifar10.cifar_spec import hyper_params_spec
 from image_classification.tf.log import log
-from image_classification.tf.mnist_spec import hyper_params_spec
 from image_classification.tf.spec import ParsedSpec
 
 __author__ = "maxim"
@@ -17,7 +17,7 @@ sampler = DefaultSampler()
 sampler.add_uniform(parsed.size())
 
 strategy_params = {
-  'io_load_dir': 'mnist/stage1-2.0',
+  'io_load_dir': '_models/cifar10/hyper/stage1-1.1',
 }
 strategy = BayesianStrategy(sampler, **strategy_params)
 print strategy.points
