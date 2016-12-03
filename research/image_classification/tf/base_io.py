fe302c19c8944491a4eee58aaa4a5dc3edc0f316
Maxim Podkolzine <2580604+maxim5@users.noreply.github.com>
2016-12-03 13:41:46 +0100

    reuse IO code

--- research/image_classification/tf/base_io.py
+++ research/image_classification/tf/base_io.py
@@ -4,7 +4,10 @@
 __author__ = 'maxim'
 
 import os
-from util import str_to_dict
+import numpy as np
+
+from logging import *
+from util import str_to_dict, dict_to_str
 
 
 class BaseIO(object):
@@ -30,3 +33,46 @@ class BaseIO(object):
         return str_to_dict(line)
     except:
       return {}
+
+
+class Serializable(object):
+  def import_from(self, data):
+    raise NotImplementedError()
+
+  def _import_from_keys(self, data, keys, default_value):
+    for key in keys:
+      value = np.array(data.get(key, default_value))
+      setattr(self, '_%s' % key, value)
+
+  def export_to(self):
+    raise NotImplementedError()
+
+  def _export_keys_to(self, keys):
+    return {key: getattr(self, '_%s' % key).tolist() for key in keys}
+
+
+class DefaultIO(BaseIO):
+  def __init__(self, serializable, filename, **params):
+    super(DefaultIO, self).__init__(**params)
+    self.serializable = serializable
+    self.filename = filename
+
+  def load(self):
+    directory = self.load_dir
+    if not directory is None:
+      destination = os.path.join(directory, self.filename)
+      if os.path.exists(destination):
+        data = DefaultIO._load_dict(destination)
+        debug('Loaded data: %s from %s' % (dict_to_str(data), destination))
+        self.serializable.import_from(data)
+        return
+    self.serializable.import_from({})
+
+  def save(self):
+    directory = self.save_dir
+    if not DefaultIO._prepare(directory):
+      return
+    destination = os.path.join(directory, self.filename)
+    with open(destination, 'w') as file_:
+      file_.write(dict_to_str(self.serializable.export_to()))
+      debug('Data saved to %s' % destination)
