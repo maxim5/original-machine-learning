0362c883dda5ccfbdb3bb5a29d1bd3db6527184e
Maxim Podkolzine <2580604+maxim5@users.noreply.github.com>
2016-12-08 21:48:49 +0100

    minor

--- research/image_classification/tf/cifar10/main.py
+++ research/image_classification/tf/cifar10/main.py
@@ -54,15 +54,19 @@ def init_augmentation(**params):
   return augmentation
 
 
+# {'conv': {1: {'activation': 'elu', 'dropout': 0.850045, 'filters': [[5, 1, 64], [1, 5, 64]], 'pools': [2, 2]}, 2: {'activation': 'relu', 'dropout': 0.907284, 'filters': [[5, 1, 105], [1, 7, 101]], 'pools': [2, 2]}, 3: {'activation': 'elu', 'dropout': 0.889042, 'filters': [[7, 1, 190], [1, 7, 214]], 'pools': [2, 2]}, 'layers_num': 3}, 'fc': {'activation': 'prelu', 'dropout': 0.699671, 'size': 439}, 'init_stdev': 0.075323, 'optimizer': {'beta1': 0.900000, 'beta2': 0.999000, 'epsilon': 1.000000e-08, 'learning_rate': 0.001575}}
+# {'conv': {1: {'activation': 'elu', 'dropout': 0.850263, 'filters': [[3, 1, 49], [1, 7, 60]], 'pools': [2, 2]}, 2: {'activation': 'relu', 'dropout': 0.856936, 'filters': [[5, 1, 109], [1, 7, 117]], 'pools': [2, 2]}, 3: {'activation': 'relu6', 'dropout': 0.816066, 'filters': [[7, 1, 223], [1, 5, 195]], 'pools': [2, 2]}, 'layers_num': 3}, 'fc': {'activation': 'elu', 'dropout': 0.956791, 'size': 489}, 'init_stdev': 0.075416, 'optimizer': {'beta1': 0.900000, 'beta2': 0.999000, 'epsilon': 1.000000e-08, 'learning_rate': 0.001385}}
+# {'conv': {1: {'activation': 'elu', 'dropout': 0.914531, 'filters': [[3, 1, 65], [1, 3, 63]], 'pools': [2, 2]}, 2: {'activation': 'leaky_relu', 'dropout': 0.874635, 'filters': [[3, 1, 158], [1, 3, 141]], 'pools': [2, 2]}, 3: {'activation': 'prelu', 'dropout': 0.879949, 'filters': [[3, 1, 188], [1, 3, 215]], 'pools': [2, 2]}, 'layers_num': 3}, 'fc': {'activation': 'relu6', 'dropout': 0.522513, 'size': 510}, 'init_stdev': 0.093356, 'optimizer': {'beta1': 0.900000, 'beta2': 0.999000, 'epsilon': 1.000000e-08, 'learning_rate': 0.001445}}
 def stage1():
   data = get_cifar10_data()
+  directory = '_models/cifar10/hyper/stage1-4.0'
 
   curve_params = {
     'burn_in': 20,
     'min_input_size': 4,
     'value_limit': 0.785,
-    'io_load_dir': '_models/cifar10/hyper/stage1-2.6',
-    'io_save_dir': '_models/cifar10/hyper/stage1-2.6',
+    'io_load_dir': directory,
+    'io_save_dir': directory,
   }
   curve_predictor = LinearCurvePredictor(**curve_params)
 
@@ -76,7 +80,7 @@ def stage1():
       'evaluate_test': True,
       'eval_flexible': False,
       'save_dir': '_models/cifar10/model-zoo/%s-%s' % (datetime.datetime.now().strftime('%Y-%m-%d'), random_id()),
-      'save_accuracy_limit': 0.79,
+      'save_accuracy_limit': 0.795,
     }
 
     model = ConvModel(input_shape=(32, 32, 3), num_classes=10, **hyper_params)
@@ -87,20 +91,20 @@ def stage1():
   strategy_params = {
     'strategy': 'portfolio',
     'methods': ['ucb', 'pi', 'rand'],
-    'probabilities': [0.05, 0.9, 0.05],
-    'io_load_dir': '_models/cifar10/hyper/stage1-2.6',
-    'io_save_dir': '_models/cifar10/hyper/stage1-2.6',
+    'probabilities': [0.1, 0.9, 0.0],
+    'io_load_dir': directory,
+    'io_save_dir': directory,
   }
 
-  tuner = HyperTuner(hyper_params_spec_2_6, solver_generator, **strategy_params)
+  tuner = HyperTuner(hyper_params_spec_4_0, solver_generator, **strategy_params)
   tuner.tune()
 
 
 def stage2(path=None, random_fork=True, batch_size=250, epochs=25):
   if not path:
-    path = read_model('_models/cifar10/model-zoo-selected')
+    path = read_model('_models/cifar10/model-zoo')
 
-  model_path = '_models/cifar10/model-zoo-selected/%s' % path
+  model_path = '_models/cifar10/model-zoo/%s' % path
 
   data = get_cifar10_data()
 
@@ -119,7 +123,8 @@ def stage2(path=None, random_fork=True, batch_size=250, epochs=25):
 
   augmentation = None
   if random_fork:
-    instance = spec.get_instance(augment_spec)
+    # instance = spec.get_instance(augment_spec)
+    instance = {'crop_size': 2, 'scale': [0.9, 1.2]}
     hyper_params.update({'augment': instance})
     augmentation = init_augmentation(**instance)
 
@@ -131,16 +136,42 @@ def stage2(path=None, random_fork=True, batch_size=250, epochs=25):
 
 # ["2016-11-29-T9FJC4"]
 # "2016-11-26-IQ8F1W", "2016-11-28-IXTV2W", "2016-11-28-GF29HW", '2016-11-28-5UUDAW', '2016-11-29-7WKH7N'
-def stage3(models =('2016-11-28-LUOHB2', )):
+def stage3(models=('2016-12-06-1ZU4GB', '2016-12-06-E7S0E1', '2016-12-06-XSKFM8', '2016-12-07-7OG78H', '2016-12-07-V0EEVZ')):
   while True:
     for path in models:
-      stage2(path, batch_size=600, epochs=10)
+      stage2(path, batch_size=500, epochs=10)
       tf_reset_all()
       import time
       time.sleep(20)
 
 
-def list_all(path='_models/cifar10/model-zoo-selected'):
+def stage4(path='_models/cifar10/model-zoo/2016-12-07-7OG78H'):
+  solver_params = {
+    'batch_size': 500,
+    'eval_batch_size': 2500,
+    'epochs': 5,
+    'evaluate_test': False,
+    'eval_flexible': False,
+    'eval_train_every': 2,
+    'eval_validation_every': 2,
+    'save_dir': path,
+    'load_dir': path,
+  }
+
+  data = get_cifar10_data()
+  data.merge_validation_to_train()
+
+  model_io = TensorflowModelIO(**solver_params)
+  hyper_params = model_io.load_hyper_params() or {}
+
+  model = ConvModel(input_shape=(32, 32, 3), num_classes=10, **hyper_params)
+  runner = TensorflowRunner(model=model)
+  augmentation = init_augmentation(**hyper_params.get('augment', {}))
+  solver = TensorflowSolver(data=data, runner=runner, augmentation=augmentation, **solver_params)
+  solver.train()
+
+
+def list_all(path='_models/cifar10/model-zoo'):
   list_models(path)
 
 
@@ -149,6 +180,7 @@ if __name__ == "__main__":
     'stage1': stage1,
     'stage2': stage2,
     'stage3': stage3,
+    'stage4': stage4,
     'list': list_all,
   }
 
