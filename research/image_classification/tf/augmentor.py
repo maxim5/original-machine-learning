34f7d3609160fd9752e177b046248256da39f369
maximp <maximp@localhost>
2016-12-09 17:55:20 +0100

    random brightness

--- research/image_classification/tf/augmentor.py
+++ research/image_classification/tf/augmentor.py
@@ -3,7 +3,6 @@
 
 __author__ = 'maxim'
 
-
 import numbers
 import random
 
@@ -12,7 +11,6 @@ import skimage.transform
 import skimage.util
 from tflearn import ImageAugmentation
 
-
 class ImageAugmentationPlus(ImageAugmentation):
   def apply(self, batch):
     return super(ImageAugmentationPlus, self).apply(list(batch))
@@ -20,11 +18,11 @@ class ImageAugmentationPlus(ImageAugmentation):
   def add_random_scale(self, downscale_limit=1.0, upscale_limit=1.0, fix_aspect_ratio=False):
     if isinstance(downscale_limit, numbers.Number):
       downscale_limit = (downscale_limit, downscale_limit)
-    downscale_limit = t_min(t_max(downscale_limit, (0, 0)), (1, 1))
+    downscale_limit = _tuple_min(_tuple_max(downscale_limit, (0, 0)), (1, 1))
 
     if isinstance(upscale_limit, numbers.Number):
       upscale_limit = (upscale_limit, upscale_limit)
-    upscale_limit = t_max(upscale_limit, (1, 1))
+    upscale_limit = _tuple_max(upscale_limit, (1, 1))
 
     if downscale_limit != (1, 1) or upscale_limit != (1, 1):
       self.methods.append(self._random_scale)
@@ -34,6 +32,12 @@ class ImageAugmentationPlus(ImageAugmentation):
     self.methods.append(self._random_swirl)
     self.args.append([strength_limit, radius_limit])
 
+  def add_random_brightness(self, min_delta, max_delta):
+    min_delta = min(min_delta, 1.0)
+    max_delta = max(max_delta, 1.0)
+    self.methods.append(self._random_brightness)
+    self.args.append([min_delta, max_delta])
+
   def _random_scale(self, batch, downscale_limit, upscale_limit, fix_aspect_ratio):
     for i in range(len(batch)):
       if bool(random.getrandbits(1)):
@@ -42,11 +46,11 @@ class ImageAugmentationPlus(ImageAugmentation):
         if fix_aspect_ratio:
           scale[1] = scale[0]
         if scale[0] < 1 or scale[1] < 1:
-          image = skimage.transform.rescale(image, scale=t_min(scale, (1, 1)), preserve_range=True)
-          image = pad_to_shape(image, batch[i].shape)
+          image = skimage.transform.rescale(image, scale=_tuple_min(scale, (1, 1)), preserve_range=True)
+          image = _pad_to_shape(image, batch[i].shape)
         if scale[0] > 1 or scale[1] > 1:
-          image = skimage.transform.rescale(image, scale=t_max(scale, (1, 1)), preserve_range=True)
-          image = crop_to_shape(image, batch[i].shape)
+          image = skimage.transform.rescale(image, scale=_tuple_max(scale, (1, 1)), preserve_range=True)
+          image = _crop_to_shape(image, batch[i].shape)
         batch[i] = image
     return batch
 
@@ -61,16 +65,25 @@ class ImageAugmentationPlus(ImageAugmentation):
         batch[i] = image
     return batch
 
+  def _random_brightness(self, batch, min_delta, max_delta):
+    for i in range(len(batch)):
+      if bool(random.getrandbits(1)):
+        image = batch[i]
+        delta = np.random.uniform(min_delta, max_delta)
+        if delta != 1:
+          image = image * delta
+          image = np.minimum(image, 1.0)
+          image = np.maximum(image, 0.0)
+          batch[i] = image
+    return batch
 
-def t_min(lhs, rhs):
+def _tuple_min(lhs, rhs):
   return min(lhs[0], rhs[0]), min(lhs[1], rhs[1])
 
-
-def t_max(lhs, rhs):
+def _tuple_max(lhs, rhs):
   return max(lhs[0], rhs[0]), max(lhs[1], rhs[1])
 
-
-def pad_to_shape(image, target_shape):
+def _pad_to_shape(image, target_shape):
   current_shape = image.shape
   diff = (target_shape[0] - current_shape[0], target_shape[1] - current_shape[1])
   side = (diff[0] / 2, diff[1] / 2)
@@ -78,7 +91,7 @@ def pad_to_shape(image, target_shape):
   return skimage.util.pad(image, pad_width=pad_width, mode='constant')
 
 
-def crop_to_shape(image, target_shape):
+def _crop_to_shape(image, target_shape):
   current_shape = image.shape
   diff = (current_shape[0] - target_shape[0], current_shape[1] - target_shape[1])
   side = (diff[0] / 2, diff[1] / 2)
